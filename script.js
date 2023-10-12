7f23a0a82e79c732bd348ac2ecd7216b547cbbc9
arnobioaires <arnobioaires2@outlook.com>
2023-10-11 22:27:36 -0300

    :tada: initial commit

---
+++ script.js
@@ -0,0 +1,36 @@
+function toogleMode() {
+    const html = document.documentElement
+    html.classList.toggle("light") 
+
+  
+    
+   /*  // pegar a tag img
+    const img = document.querySelector("#profile img")
+
+    //substituir a imagem
+
+    if(html.classList.contains("light")) {
+        //se tiver light mode, adiconar a imagem light
+
+        img.setAttribute("src", "./assets/avatar-light.png")
+    } else {
+        img.setAttribute("src", "https://github.com.br/arnobioaires.png") */
+    }
+
+ 
+ 
+ 
+
+
+ // documentElement representa o documento, ou seja a tag html;
+ //https://serviceit.vagas.solides.com.br/?page=2
+ /* 
+ 
+ segundo o MGI, as provas objetivas de aplicação geral serão compostas por temas ligados 
+ à valorização do ethos público, da realidade brasileira e das relações entre estado,
+  políticas públicas e desenvolvimento nacional, e disciplinas curriculares tradicionais em 
+  concursos públicos, que incluem, por exemplo, conhecimentos referentes às bases constitucionais do estado,
+ à estrutura e funcionamento da administração pública, bem como aos fundamentos do direito administrativo.
+ 
+ */
+ 
